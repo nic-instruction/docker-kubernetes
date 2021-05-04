fdacfbebb6e7e032e602d67a771e2e90c18b59b0
nic-instruction <nicole.bade@seattlecolleges.edu>
2021-05-04 12:47:42 -0700

    Update gitlab.sh

--- gitlab/gitlab.sh
+++ gitlab/gitlab.sh
@@ -33,5 +33,18 @@ chmod 400 /root/gitlabAdminPass
 # curl -k -L --request GET https://$externalIP
 # -k tells curl to ignore the self signed cert, and -L tells it to follow the redirect
 
+# use the admin pass to log in and set a new admin pass via curl (have not implemented yet, because it's not pressing)
+
+# If you need a password for automation and to create runners without setting an admin pass take a look at /etc/gitlab/gitlab.rb
+# get defulat admin pass with:
+# sed -i"" "s#\# gitlab_rails\['initial_root_password.*#gitlab_rails\['initial_root_password'\] = \""${GITLAB_INITIAL_ROOT_PASSWD}"\"#g" /etc/gitlab/gitlab.rb
+# or, for more security, set your own, but you'll really want to make this super temporary, even for automation.
+
+# Now you can go and log in!  Fun times.
+# though if you're on google cloud you will still need to configure postgres to use a different port.
+
+
+
+
 
 
