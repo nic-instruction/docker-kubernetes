598f2ca9b1c3759fbe55579ee39b4a724900011f
nic-instruction <nicole.bade@seattlecolleges.edu>
2021-05-04 16:16:52 -0700

    Update build.sh

--- docker/project/build.sh
+++ docker/project/build.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+# by default centos 7 comes with systemd but needs some mods to run correctly
+# we'll take care of that first, using a custom dockerfile in centos_with_systemd
 
+# start by pulling down the centos7 image
 docker pull centos:7
+
+# change into the subdir
+cd centos_with_systemd/Dockerfile
+
+docker build --rm -t local/c7-systemd .
