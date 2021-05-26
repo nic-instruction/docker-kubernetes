254e09bccdef10763fdd0dda40215cba743a479f
nic-instruction <nicole.bade@seattlecolleges.edu>
2021-05-25 17:22:57 -0700

    Update registry_setup.sh

--- kubernetes-docker-gcloud/registry_setup.sh
+++ kubernetes-docker-gcloud/registry_setup.sh
@@ -9,6 +9,22 @@ gcloud artifacts repositories list
 
 gcloud auth configure-docker us-central1-docker.pkg.dev --quiet
 
+git clone https://github.com/nic-instruction/docker-kubernetes.git
+
+cd docker-kubernetes/docker/project/sucessfull-attempt/
+
+docker build . -t appsrv:v1
+
+# $GOOGLE_CLOUD_PROJECT is the environmental variable that holds your current google cloud project ID
+
+docker tag docker.io/library/appsrv:v1 \
+us-central1-docker.pkg.dev/$GOOGLE_CLOUD_PROJECT/quickstart-docker-repo/appsrv:tag1
+
+docker push us-central1-docker.pkg.dev/$GOOGLE_CLOUD_PROJECT/quickstart-docker-repo/appsrv:tag1
+
+docker pull us-central1-docker.pkg.dev/$GOOGLE_CLOUD_PROJECT/quickstart-docker-repo/appsrv:tag1
+
+
 
 
 
